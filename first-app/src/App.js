9c05c10d8c50c3eb152ed7563e0acb3b05455d13
unknown <evaseregina04@gmail.com>
2023-04-01 21:04:30 +0300

    homework2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,80 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './header';
+import { Contacts } from './contacts';
 
 export default function App() {
   return (
     <>
       <div class="all">
-        <Header />
-        <section class="section">
-          <div class="section__info">
-            <form class="section__form" action="login" method="post">
-              <h2 class="section__title">Войти на сайт</h2>
-              <p class="section__login"><input class="section__log" type="text" placeholder="Логин" required />
-              </p>
-              <p class="section__password"><input class="section__password2" type="password" placeholder="Пароль"
-                required />
-              </p>
-              <button type="submit" class="section__btn section__btn-login">Войти</button>
-            </form>
-          </div>
-        </section>
+        <header class="header"> <a class="header__logo" href="index.html">
+          <img class="header__image1" src="img/logo.svg" alt="logotype" /> </a>
+          <nav class="header__nav" />
+          <ul class="header__menu">
+            <li class="header__menu-list"><a class="header__menu-link " href="index.html">Главная</a>
+            </li>
+            <li class="header__menu-list"><a class="header__menu-link" href="students.html">Студенты</a></li>
+            <li class="header__menu-list"><a class="header__menu-link" href="about.html">О курсе</a></li>
+            <li class="header__menu-list"><a class="header__menu-link header__menu-link_active"
+              href="contacts.html">Контакты</a></li>
+          </ul>
+          <nav />
+          <a class="header__button" href="#">Войти</a>
+        </header>
         <div class="main">
-          <main class="main__content">
-            <h1 class="main__title">Добро пожаловать на курс “Создание веб-сайтов (HTML, CSS)”!</h1>
-            <img class="main__image" src="img/contentphoto.jpg" alt="Главное фото" />
-            <div class="main__content-info">
-              <div class="main__content-section">
-                <div class="main__content-article">
-                  <h3 class="main__subtitle">Для кого этот курс</h3>
-                  <p class="main__about-text">Этот курс рассчитан на новичков, которые только начинают
-                    знакомство
-                    с
-                    веб-разработкой. В начале курса вы узнаете о том, как устроен Интернет и веб-сайты.</p>
-                  <p class="main__about-text">Для обучения необходимо только владеть компьютерной грамотностью
-                    (пользоваться браузером, создавать папки и файлы, устанавливать программы).</p>
-
-                </div>
-
-                <div class="main__content-article">
-                  <h3 class="main__subtitle">Чему вы научитесь</h3>
-                  <p class="main__about-text ">На этом курсе вы узнаете, как устроены веб-сайты и как
-                    создавать
-                    их с нуля.
-                    Изучите основы языков <strong>HTML</strong> ,<strong>CSS</strong> и немного
-                    <strong>JavaScript</strong>. Мы вместе сверстаем сайт нашей группы по этому
-                    макету. А затем вы <em class="main__detail">сами</em> сверстаете свои личные странички в
-                    качестве
-                    домашнего задания и
-                    мы
-                    выложим
-                    все это на сервер.
-                  </p>
-                </div>
-              </div>
-              <h2 class="main__heading">“Учимся вместе!”</h2>
-            </div>
-          </main>
+          <main class="main__content-contacts" />
+          <h1 class="main__heading-contacts">Как нас найти </h1>
+          <div class="main__map-contacts"><iframe class="main__map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d511634.70962970407!2d29.534289435013047!3d59.93922591096038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696378cc74a65ed%3A0x6dc7673fab848eff!2z0KHQsNC90LrRgi3Qn9C10YLQtdGA0LHRg9GA0LM!5e0!3m2!1sru!2sru!4v1677008804773!5m2!1sru!2sru"
+            style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+          <Contacts />
         </div>
       </div>
       <footer class="footer">
         <p class="footer__text">2023 &copy; <a class="footer__work-centr" href="#">учебный центр</a></p>
       </footer>
-      {/* { <script>
-    const buttonEl = document.querySelector('.header__button');
-    const blockPopup = document.querySelector('.section');
-    const secondButton = document.querySelector('.section__btn');
-
-    buttonEl.addEventListener('click', function (e) {
-        blockPopup.classList.toggle('visible');
-    });
-    secondButton.addEventListener('click', () => {
-        blockPopup.classList.toggle('visible');
-    });
-</script> } */}
-
-    </>
-
-  );
+    </>);
 }
---
+++ first-app/src/contacts.js
@@ -0,0 +1,13 @@
+export function Contacts() {
+    return { 
+        <>
+        <div class="main__info-contacts">
+            <h3 class="main__our-contacts">Наши контакты</h3>
+            <div class="main__adress-info">
+                <p class="main__adress">Адрес: г. Санкт-Петербург, Дворцовая пл., 2, 190000 <br />
+                    <p> <a class="main__adress" href="tel:+7(812)5555555">Тел: +7 (812) 555-55-55</a></p>
+                </p>
+            </div>
+        </div>
+}
+}
